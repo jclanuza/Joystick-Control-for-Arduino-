28ca20d57ef9cb7d4cd177a64a91e412bcddeaa0
Juan Carlos Lanuza Lazo <jclanuza@icloud.com>
2016-12-14 15:24:28 -0600

    Add files via upload

---
+++ Control_Joystick.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+Servo X;
+Servo Y;
+
+void setup() {
+  X.attach(10);
+  Y.attach(9);
+}
+
+void loop() {
+  X.write(map(analogRead(A0),0,1023,0,180));
+  Y.write(map(analogRead(A1),0,1023,0,180));
+}
